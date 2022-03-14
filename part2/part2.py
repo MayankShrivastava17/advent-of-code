4b826837c6727d9f984654936656f2680e5f6d1d
MayankShrivastava17 <mayanksri910@gmail.com>
2022-03-15 00:13:45 +0530

    Added part 2 of day 17

---
+++ part2/part2.py
@@ -0,0 +1,27 @@
+import sys
+from iparser import read_input
+
+def simulate(vel_x, vel_y, x_min, x_max, y_min, y_max):
+    x = y = 0
+    while abs(x) <= max(abs(x_min), abs(x_max)) and y >= y_min:
+        if x_min <= x <= x_max and y_min <= y <= y_max:
+            return 1
+        x += vel_x
+        y += vel_y
+        vel_x += [-1, 0, 1][(vel_x < 0) + (vel_x <= 0)]
+        vel_y -= 1
+    return 0
+
+def process(data):
+    (x_min, x_max), (y_min, y_max) = data
+    total = 0
+    for vel_x in range(0, max(x_max, 0) + 1):
+        for vel_y in range(y_min, abs(y_max) * 2 + 1):
+            total += simulate(vel_x, vel_y, x_min, x_max, y_min, y_max)
+    return total
+
+if __name__ == '__main__':
+    input_file =  'input.txt'
+    input_data = read_input(input_file)
+    result = process(input_data)
+    print(result)
